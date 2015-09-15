ae54a6ca813c4a5508cf07fc77e225aa3c4836a9
apl2 <apl2of2@gmail.com>
2015-09-15 09:44:18 -0500

    Back for screenshots

--- DigIt3/src/com/dig/www/start/Board.java
+++ DigIt3/src/com/dig/www/start/Board.java
@@ -98,8 +98,8 @@ public class Board extends MPanel implements ActionListener {
 	public Board(DigIt dM, String name, int which) {
 		character = new Spade(Statics.BOARD_WIDTH / 2 - 50, Statics.BOARD_HEIGHT / 2 - 50, this);
 
-		world = StageBuilder.getInstance().read("map1", this);
-		enemies = StageBuilder.getInstance().loadEn("map1",this);
+		world = StageBuilder.getInstance().read("funLittleMap", this);
+		enemies = StageBuilder.getInstance().loadEn("funLittleMap",this);
 		for (int c = 0; c < enemies.size(); c++) {
 			enemies.get(c).setAlive(true);
 
