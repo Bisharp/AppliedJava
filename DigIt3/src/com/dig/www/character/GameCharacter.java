3aa86d861110ed8ae418439891ec864f81f44886
apl2 <apl2of2@gmail.com>
2015-10-10 22:23:02 -0500

    Removed a print statement.

--- DigIt3/src/com/dig/www/character/GameCharacter.java
+++ DigIt3/src/com/dig/www/character/GameCharacter.java
@@ -218,7 +218,7 @@ if(path.getPoints().size()>0&&new Point(x,y).distance(path.getCurrentFind())<11)
 	path.removeLast();
 }
 if(new Point(x, y).distance(owner.getCharPoint())<140){
-	System.out.println(getType().charName()+" CLOSE:"+new Date());
+	//System.out.println(getType().charName()+" CLOSE:"+new Date());
 	path=null;
 }
 }
