c4dc3bd120900fd744ad81e1fa700092bf2a9fe2
apl2 <apl2of2@gmail.com>
2015-10-16 11:21:57 -0500

    Messing with LizardMan

--- DigIt3/src/com/dig/www/enemies/LizardMan.java
+++ DigIt3/src/com/dig/www/enemies/LizardMan.java
@@ -15,7 +15,7 @@ int lastY;
 int bMove;
 	public LizardMan(int x, int y, Board owner) {
 		super(x, y, "images/enemies/unique/jello-O.png", owner, true, 99,
-				"Lizard-Man of Doom", 2, "music/zeldaCopyright2.mp3",
+				"Lizard-Man of Doom", 5, "music/zeldaCopyright2.mp3",
 				"gunSFX/explosion-2.wav", "enemy/LizHurt.wav");
 		// TODO Auto-generated constructor stub
 	}
@@ -29,17 +29,22 @@ int bMove;
 	@Override
 	public void animate() {
 		// TODO Auto-generated method stub
-		if(health<(maxHealth/3)&&phase<2){
-			phase=2;
-			Statics.playSound(owner,bossPhaseS);
-			owner.getEnemies().add(new Explosion(x, y, "images/effects/shadow.png", owner));
-		phaseA=true;
-		}if(health<(maxHealth/3)*2&&phase<1){
-				phase=1;
-				Statics.playSound(owner,bossPhaseS);
-				owner.getEnemies().add(new Explosion(x, y, "images/effects/shadow.png", owner));
-				phaseA=true;		
-		}
+//		if(health<(maxHealth/3)&&phase<2){
+//			phase=2;
+//			followTimer=0;
+//			attackNum=-1;
+//			Statics.playSound(owner,bossPhaseS);
+//			owner.getEnemies().add(new Explosion(x, y, "images/effects/shadow.png", owner));
+//		phaseA=true;
+//		}if(health<(maxHealth/3)*2&&phase<1){
+//				phase=1;
+//
+//				followTimer=0;
+//				attackNum=-1;
+//				Statics.playSound(owner,bossPhaseS);
+//				owner.getEnemies().add(new Explosion(x, y, "images/effects/shadow.png", owner));
+//				phaseA=true;		
+//		}
 		if((health<(maxHealth/6)*5)&&phase==0&&phaseA){
 			phaseA=false;
 			Statics.playSound(owner,bossPhaseS);
@@ -74,13 +79,25 @@ int bMove;
 			acted=true;}else{
 				if(actTimer<=0){
 					if(bMove==0){
-						int xPoint=(int)(15.5*100);
-						int yPoint=Math.max(y, (int)(owner.getWorld().get(0).getY()+((maxHealth/health)*(-owner.getWorld().get(0).getY()+owner.getWorld().get(owner.getWorld().size()-1).getY()))));
+						int xPoint=(int)(owner.getWorld().get(0).getX()+(15.5*100));
+						int yPoint=(int) Math.min(y, (int)(owner.getWorld().get(0).getY()+
+								((double)((double)health/(double)maxHealth)
+								*(-owner.getWorld().get(0).getY()+owner.getWorld().get(owner.getWorld().size()-1).getY()))));
 						moveTo(xPoint,yPoint, 1, 10);
+						bMove++;
+			
+						System.out.println("H: "+((double)health/(double)maxHealth));
+						System.out.println("Min: "+(int)(owner.getWorld().get(0).getY()));
+						System.out.println("Max: "+(int)(owner.getWorld().get(owner.getWorld().size()-1).getY()));
+						System.out.println("My: "+yPoint);
+						
 					}
-					if(bMove<3){
+					else	if(bMove<3){
 					createProjectile("images/enemies/blasts/0.png", 10, Statics.pointTowards(new Point((int) x,
 						(int) y), new Point(owner.getCharacterX()+40,owner.getCharacterY()+40)), false, 20);
+					bMove++;
+					}else{
+						bMove=0;
 					}
 				}
 			}
@@ -94,7 +111,7 @@ int bMove;
 		
 	}
 	public boolean sortAction(){
-		if(attackNum==0){
+		if(attackNum==2){
 			moveDForward();
 			for(Block b: owner.getWorld()){
 				if(!b.traversable()&&b.getBounds().intersects(getBounds())){
@@ -120,6 +137,15 @@ int bMove;
 				speedMulti=1;
 			}
 			return true;
+		}else if(attackNum==3){
+			moveDForward();
+			
+			followTimer--;
+			if(followTimer==0){
+				attackNum=-1;
+				speedMulti=1;
+			}
+			return true;
 		}
 
 			
@@ -132,9 +158,13 @@ int bMove;
 		this.speedMulti=speedMulti;
 		this.followTimer=max;
 		actTimer=timer;
-		attackNum=0;
+		attackNum=2;
 	}
 	public void moveTo(int moveX,int moveY,double multiSpeed,int timer){
-		
+		Point myPoint=new Point(x,y);
+		Point thePoint=new Point(moveX,moveY);
+		diagnalMove((int)Statics.pointTowards(myPoint, thePoint),(int) ((int)myPoint.distance(thePoint)/(speed*multiSpeed)),multiSpeed,timer);
+	
+	attackNum=3;
 	}
 }
