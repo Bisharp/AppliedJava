ba4f46582c0944224eec8484a03b6d8e9295e976
Bisharp <micah.schiewe@gmail.com>
2015-10-17 00:31:38 -0500

    Added 'boss' portal image file; if we don't like it, we can change it later. Changed LuigisMansion's portal to use the boss skin. Modified field so that the rectangles are only drawn within its circular bounds; will later add support so that the computer will use these bounds for deflection and healing as well.

--- DigIt3/src/com/dig/www/character/Field.java
+++ DigIt3/src/com/dig/www/character/Field.java
@@ -2,6 +2,8 @@ package com.dig.www.character;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.Rectangle;
 
 import com.dig.www.start.Board;
 import com.dig.www.util.Sprite;
@@ -10,6 +12,7 @@ import com.dig.www.util.Statics;
 public class Field extends FProjectile {
 
 	public static final int SIDE = Statics.BLOCK_HEIGHT * 3;
+	private Polygon field;
 
 	public static final Color FIELD0 = // new Color(63, 72, 204, 100);
 	new Color(255, 0, 255, 50);
@@ -30,12 +33,37 @@ public class Field extends FProjectile {
 
 		width = SIDE;
 		height = SIDE;
+
+		field();
+	}
+
+	private void field() {
+		int[] xs = new int[] { x,
+				x + SIDE / 6,
+				x + SIDE / 2,
+				x + SIDE - SIDE / 6,
+				x + SIDE,
+				x + SIDE - SIDE / 6,
+				x + SIDE / 2,
+				x + SIDE / 6,
+				};
+		int[] ys = new int[] { y + SIDE / 2,
+				y + SIDE / 10,
+				y,
+				y + SIDE / 10,
+				y + SIDE / 2,
+				y + SIDE - SIDE / 10,
+				y + SIDE,
+				y + SIDE - SIDE / 10,
+				};
+
+		field = new Polygon(xs, ys, xs.length);
 	}
 
 	public void draw(Graphics2D g2d) {
 
 		g2d.setColor(getColor());
-		g2d.fillOval(x, y, SIDE, SIDE);
+		g2d.fillOval(x - 10, y - 10, SIDE + 20, SIDE + 20);
 
 		vars = new int[4][2];
 		for (int i = 0; i < 4; i++) {
@@ -45,12 +73,19 @@ public class Field extends FProjectile {
 		}
 
 		g2d.setColor(Color.WHITE);
-		for (int[] var : vars)
-			g2d.fillRect(var[0], var[1], 10, 10);
+		for (int[] var : vars) {
+			Rectangle rect = new Rectangle(var[0], var[1], 10, 10);
+
+			if (field.intersects(rect))
+				g2d.fill(rect);
+		}
+		
+//		g2d.setColor(Color.blue);
+//		g2d.draw(field);
 	}
 
 	private int getVal(boolean x) {
-		return (x ? getMidX() : getMidY()) + (Statics.RAND.nextInt(width / 2) * (Statics.RAND.nextBoolean() ? 1 : -1));
+		return (x ? getMidX() : getMidY()) + (Statics.RAND.nextInt((int) (SIDE / 2)) * (Statics.RAND.nextBoolean() ? 1 : -1)) - 5;
 	}
 
 	private Color getColor() {
@@ -76,5 +111,10 @@ public class Field extends FProjectile {
 
 	public void dBasicAnimate() {
 		super.basicAnimate();
+	}
+
+	public void animate() {
+		super.animate();
+		field();
 	}
 }
