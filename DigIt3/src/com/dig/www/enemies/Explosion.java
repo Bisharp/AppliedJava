68d42fb0f41d95369c27cf52afe879f7a211f433
Bisharp <micah.schiewe@gmail.com>
2015-10-22 16:07:53 -0500

    Added up/down images for LizardMan; also made it so that Explosions cannot be stunned.

--- DigIt3/src/com/dig/www/enemies/Explosion.java
+++ DigIt3/src/com/dig/www/enemies/Explosion.java
@@ -1,21 +1,28 @@
 package com.dig.www.enemies;
 
+import com.dig.www.character.GameCharacter;
+import com.dig.www.character.Moves;
 import com.dig.www.start.Board;
 
-public class Explosion extends Enemy{
+public class Explosion extends Enemy {
+	
 	private int boomTimer = 25;
+
 	public Explosion(int x, int y, String loc, Board owner) {
 		super(x, y, loc, owner, true, -10);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void animate() {
-		// TODO Auto-generated method stub
 		basicAnimate();
 		boomTimer--;
 
 		if (boomTimer <= 0)
 			alive = false;
+	}
+
+	@Override
+	public void interact(Moves move, GameCharacter chr, boolean fromP) {
+
 	}
 }
