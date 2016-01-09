0606b783ef732e1d858021719cb6682bc42c8836
Bisharp <micah.schiewe@gmail.com>
2016-01-09 14:57:28 -0600

    Another GameController related class which got missed in the last commit.

---
+++ DigIt3/src/com/dig/www/util/GameControllerPreferences.java
@@ -0,0 +1,15 @@
+package com.dig.www.util;
+
+import java.io.Serializable;
+
+public class GameControllerPreferences implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private int moveX = GameControllerRunnable.X_STICK;
+	private int moveY = GameControllerRunnable.Y_STICK;
+	
+}
