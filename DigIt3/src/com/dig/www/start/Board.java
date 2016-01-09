34d4a02a1d283c479b7bb51c2b735444fdf084a1
Bisharp <micah.schiewe@gmail.com>
2016-01-09 15:03:01 -0600

    Added the getWallList() method.

--- DigIt3/src/com/dig/www/start/Board.java
+++ DigIt3/src/com/dig/www/start/Board.java
@@ -90,7 +90,8 @@ public class Board extends MPanel implements ActionListener {
 	private final int DOORSTATETMAX = 50;
 	private String doorStateLev = "";
 	private int spawnNum;
-private Point spawnLoc;
+	private Point spawnLoc;
+
 	public enum State {
 		INGAME, PAUSED, QUIT, SHOP, LOADING, DEAD, NPC, DOOROPEN;
 	};
@@ -175,9 +176,11 @@ private Point spawnLoc;
 
 	private int startPoint = 0;
 	private Time time;
-public int getStartPoint(){
-	return startPoint;
-}
+
+	public int getStartPoint() {
+		return startPoint;
+	}
+
 	public ArrayList<Block> getWorld() {
 		return world;
 	}
@@ -337,9 +340,9 @@ public int getStartPoint(){
 		for (Objects n : objects)
 			n.initialAnimate(spawnX, spawnY);
 
-		if(spawnLoc!=null){
-			spawnLoc.x -= spawnX-Statics.BOARD_WIDTH / 2 - 50+100;
-			spawnLoc.y -= spawnY-Statics.BOARD_HEIGHT / 2 - 50+100;
+		if (spawnLoc != null) {
+			spawnLoc.x -= spawnX - Statics.BOARD_WIDTH / 2 - 50 + 100;
+			spawnLoc.y -= spawnY - Statics.BOARD_HEIGHT / 2 - 50 + 100;
 		}
 		changeWeather();
 		updateBackground();
@@ -807,13 +810,13 @@ public int getStartPoint(){
 		// char[] names = {'S', 'C', 'D', 'H'};
 		String decision;
 
-		decision = ((String) JOptionPane.showInputDialog(this, (character.isDead()?"Your current character has been defeated.\n":"")+"Please select a character: ", DigIt.NAME, JOptionPane.PLAIN_MESSAGE, Statics.ICON,
-				getCharacters(), null));
+		decision = ((String) JOptionPane.showInputDialog(this, (character.isDead() ? "Your current character has been defeated.\n" : "")
+				+ "Please select a character: ", DigIt.NAME, JOptionPane.PLAIN_MESSAGE, Statics.ICON, getCharacters(), null));
 
 		if (decision == null) {
 			timer.restart();
 			time.resume();
-			if(character.isDead()){
+			if (character.isDead()) {
 				openSwitchDialogue();
 			}
 			return;
@@ -838,7 +841,7 @@ public int getStartPoint(){
 	}
 
 	private String[] getCharacters() {
-ArrayList<GameCharacter>friends=getAliveFriends();
+		ArrayList<GameCharacter> friends = getAliveFriends();
 		ArrayList<String> s0 = new ArrayList<String>();
 
 		for (GameCharacter friend : friends)
@@ -907,13 +910,8 @@ ArrayList<GameCharacter>friends=getAliveFriends();
 		}
 		return s1;
 	}
-<<<<<<< HEAD
 
-	private void scroll(int x, int y) {
-=======
-	
-public void scroll(int x, int y) {
->>>>>>> branch 'master' of https://github.com/Bisharp/AppliedJava.git
+	public void scroll(int x, int y) {
 		character.setX(character.getX() + x);
 		character.setY(character.getY() + y);
 
@@ -923,10 +921,10 @@ public void scroll(int x, int y) {
 		}
 		if (pointedPoint != null)
 			pointedPoint.setLocation(pointedPoint.getX() + x, pointedPoint.getY() + y);
-	
+
 		if (spawnLoc != null)
 			spawnLoc.setLocation(spawnLoc.getX() + x, spawnLoc.getY() + y);
-		
+
 		for (GameCharacter b : friends) {
 			b.setX(b.getX() + x);
 			b.setY(b.getY() + y);
@@ -1536,7 +1534,7 @@ public void scroll(int x, int y) {
 				if (n.interact()) {
 					hasTalked = true;
 					if (n instanceof CheckPoint) {
-						spawnLoc=new Point(n.getX(),n.getY());
+						spawnLoc = new Point(n.getX(), n.getY());
 						save(((CheckPoint) n).getSpawnNum());
 						System.out.println("SAVED");
 					} else if (n instanceof Collectible && ((Collectible) n).collectible())
@@ -1743,6 +1741,10 @@ public void scroll(int x, int y) {
 
 	public State getState() {
 		return state;
+	}
+	
+	public ArrayList<Block> getWallList() {
+		return wallList;
 	}
 
 	public Rectangle getScreen() {
@@ -2203,20 +2205,24 @@ public void scroll(int x, int y) {
 	public int getSpawnNum() {
 		return spawnNum;
 	}
-	public Point getSpawnLoc(){
+
+	public Point getSpawnLoc() {
 		return spawnLoc;
 	}
-	public void setSpawnLoc(Point p){
-		spawnLoc=p;
+
+	public void setSpawnLoc(Point p) {
+		spawnLoc = p;
 	}
-	public ArrayList<GameCharacter>getAliveFriends(){
-		ArrayList<GameCharacter>alive=new ArrayList<GameCharacter>();
-		for(GameCharacter c:friends)
-			if(!c.isDead())
+
+	public ArrayList<GameCharacter> getAliveFriends() {
+		ArrayList<GameCharacter> alive = new ArrayList<GameCharacter>();
+		for (GameCharacter c : friends)
+			if (!c.isDead())
 				alive.add(c);
 		return alive;
 	}
-	public void setSwitching(boolean b){
-		switching=b;
+
+	public void setSwitching(boolean b) {
+		switching = b;
 	}
 }
