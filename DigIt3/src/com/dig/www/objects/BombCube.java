3ce1ac096c3880a813fb5be4f3814e5a667f050c
apl2 <apl2of2@gmail.com>
2016-01-20 20:37:57 -0600

    Renamed companionCube image to bombCube.

--- DigIt3/src/com/dig/www/objects/BombCube.java
+++ DigIt3/src/com/dig/www/objects/BombCube.java
@@ -5,7 +5,7 @@ import com.dig.www.start.Board;
 public class BombCube extends PushCube{
 
 	public BombCube(int x, int y,boolean temp, Board owner) {
-		super(x, y, "images/objects/companionCube.png", owner, "companionCube");
+		super(x, y, "images/objects/bombCube.png", owner, "companionCube");
 		// TODO Auto-generated constructor stub
 	}
 
